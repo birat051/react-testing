474f65092ddb5a69b14d9bcfdd5b7086c118dda3
birat051 <birat.bhattacharjee_ma@srmuniv.edu.in>
2023-09-10 16:38:18 +0530

    describe block example with todo footer

--- src/components/TodoFooter/__test__/TodoFooter.test.jsx
+++ src/components/TodoFooter/__test__/TodoFooter.test.jsx
@@ -10,26 +10,29 @@ const MockFooter=({numberOfIncompleteTasks})=>{
     )
 }
 
-it('should render the correct number of todo tasks',() => {
-  render(<MockFooter numberOfIncompleteTasks={5} />)
-  const paragraphElement=screen.getByText(/5 tasks left/i)
-  expect(paragraphElement).toBeInTheDocument()
+describe("Tests for todo footer",()=>{
+    it('should render the correct number of todo tasks',() => {
+        render(<MockFooter numberOfIncompleteTasks={5} />)
+        const paragraphElement=screen.getByText(/5 tasks left/i)
+        expect(paragraphElement).toBeInTheDocument()
+      })
+      
+      it('should render the "task" when number of todo task is 1',() => {
+          render(<MockFooter numberOfIncompleteTasks={1} />)
+          const paragraphElement=screen.getByText(/1 task left/i)
+          expect(paragraphElement).toBeInTheDocument()
+      })
+      
+      it('should render the "tasks" when number of todo task is more than 1',() => {
+          render(<MockFooter numberOfIncompleteTasks={2} />)
+          const paragraphElement=screen.getByTestId('para')
+          expect(paragraphElement).toHaveTextContent("2 tasks left")
+      })
+      
+      it('should render the "task" when number of todo task is 1 in the paragraph value',() => {
+          render(<MockFooter numberOfIncompleteTasks={1} />)
+          const paragraphElement=screen.getByTestId('para')
+          expect(paragraphElement.textContent).toBe("1 task left")
+      })
 })
 
-it('should render the "task" when number of todo task is 1',() => {
-    render(<MockFooter numberOfIncompleteTasks={1} />)
-    const paragraphElement=screen.getByText(/1 task left/i)
-    expect(paragraphElement).toBeInTheDocument()
-})
-
-it('should render the "tasks" when number of todo task is more than 1',() => {
-    render(<MockFooter numberOfIncompleteTasks={2} />)
-    const paragraphElement=screen.getByTestId('para')
-    expect(paragraphElement).toHaveTextContent("2 tasks left")
-})
-
-it('should render the "task" when number of todo task is 1 in the paragraph value',() => {
-    render(<MockFooter numberOfIncompleteTasks={1} />)
-    const paragraphElement=screen.getByTestId('para')
-    expect(paragraphElement.textContent).toBe("1 task left")
-})
